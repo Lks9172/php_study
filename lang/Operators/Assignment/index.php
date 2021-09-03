c00025a93bf93c366ebe7672a96329e075837460
Lks9172 <ekdan9172@gmail.com>
2021-09-04 00:06:31 +0900

    1.5: Opserators(2)
    - 증감 연산자
    - 연산자의 우선순위
    - 대입 연산자

---
+++ lang/Operators/Assignment/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Assignment Operators.
+ *
+ * =, +=, -=, /=, *=, %=, **=, &=, |=, ^=, <<=, >>=, .=
+ */
+
+ $message = 'Hello, ';
+ echo $message; // -> Hello, 
+ echo "\n"; // -> Hello, 
+ $message .= 'world';
+
+ echo $message; // -> Hello, world
